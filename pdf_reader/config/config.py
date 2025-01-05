ffd66126de43056d8e1d1319d3a698977c05442f
NegaoDoTi <viniciusluciano2012@hotmail.com>
2025-01-05 15:49:27 -0300

    Removendo a gambiarra de env do config.py

--- pdf_reader/config/config.py
+++ pdf_reader/config/config.py
@@ -3,7 +3,7 @@ from os import getenv
 
 load_dotenv()
 
-RABBIT_HOST = "localhost"
+RABBIT_HOST = getenv("RABBIT_HOST")
 RABBIT_USER = getenv("RABBIT_USER")
 RABBIT_PASSWORD = getenv("RABBIT_PASSWORD")
 RABBIT_PORT = getenv("RABBIT_PORT")
